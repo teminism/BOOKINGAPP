d9bbe45fe7d1e44cf81eded21a713dad1d020930
Temidayo Agbeniyi <tagben95@gmail.com>
2023-09-26 22:22:50 +0100

    Node.js MongoDB CRUD operations

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
 const app = express()
 dotenv.config()
 
@@ -16,13 +20,14 @@ const connect = async () => {
 mongoose.connection.on("disconnected", ()=>{
     console.log("mongoDB disconnected!")
 })
-mongoose.connection.on("connected", ()=>{
-    console.log("mongoDB connected!")
-})
 
-app.get ("/", (req, res) => {
-    res.send("hello first request.")
-})
+//middlewares
+app.use(express.json())
+
+app.use("/api/auth", authRoute)
+app.use("/api/users", usersRoute)
+app.use("/api/hotels", hotelsRoute)
+app.use("/api/rooms ", roomsRoute)
 
 app.listen(8800, () => {
     connect()
