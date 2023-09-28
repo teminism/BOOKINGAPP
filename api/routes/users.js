451e7a0094ab21caed6dcdb2ad2e5f7d37937879
Temidayo Agbeniyi <tagben95@gmail.com>
2023-09-28 22:53:26 +0100

    authenication verification v2

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,18 +1,23 @@
 import express from "express";
 import {updateUser, deleteUser, getUser, getUsers} from "../controllers/userController.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+    res.send("Hello user, you are logged in!")
+})
+
 //UPDATE
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 
 //DELETE
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 
 //GET
-router.get("/:id", getUser);
+router.get("/:id", verifyUser, getUser);
 
 //GET ALL
-router.get("/", getUsers);
+router.get("/", verifyAdmin, getUsers);
 
 export default router
